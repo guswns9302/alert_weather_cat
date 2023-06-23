1a1fe1f0cd7677bc49ec64492dd0501d9ccf6938
yunhyunjun <guswns930213@gmail.com>
2023-06-24 01:36:44 +0900

    deploy csv read path change

--- src/main/java/com/exithere/rain/controller/CsvController.java
+++ src/main/java/com/exithere/rain/controller/CsvController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +33,8 @@ public class CsvController {
         List<String> headerList = new ArrayList<String>();
 
         try{
-            BufferedReader br = Files.newBufferedReader(Paths.get("/Users/yunhyunjun/Downloads/test.csv"));
+
+            BufferedReader br = Files.newBufferedReader(Paths.get("/home/ubuntu/alert_weather_cat/region_list.csv"));
             String line = "";
 
             while((line = br.readLine()) != null){
